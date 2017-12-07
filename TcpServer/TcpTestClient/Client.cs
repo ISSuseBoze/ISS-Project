8f8602332eb41120ea206b871aa02cc767c2801f
jurko5 <juraj.doncevic@outlook.com>
2017-12-07 14:04:23 +0100

    Add TcpServer/TestClient

---
+++ TcpServer/TcpTestClient/Client.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TcpTestClient
+{
+	public class Client
+	{
+		public string IP { get; set; }
+		public int Port { get; set; }
+
+		private TcpClient tcpClient;
+
+		public Client(string ip, int port) 
+		{
+			IP = ip;
+			Port = port;
+
+			tcpClient = new TcpClient(IP, Port);
+		}
+
+		public void sendMessage(string message)
+		{
+			byte[] bytes = System.Text.Encoding.UTF8.GetBytes(message);
+
+			tcpClient.Client.Send(bytes);
+		}
+	}
+}
---
+++ TcpServer/TcpTestClient/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TcpTestClient
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private Client client;
+
+		public MainWindow()
+		{
+			client = new Client("127.0.0.1", 1111);
+
+			InitializeComponent();
+		}
+
+		private void btn_send_Click(object sender, RoutedEventArgs e)
+		{
+			string message = tbx_message.Text;
+
+			client.sendMessage(message);
+		}
+	}
+}
