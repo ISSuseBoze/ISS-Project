84d23624621579ed553f6616bbb25404ba9cf665
zlatko_hrastic <zlatko.hr8@gmail.com>
2017-12-17 18:13:22 +0100

    Initial MVC commit

---
+++ TaskCreator/IssTaskCreator/IssTaskCreator/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IssTaskCreator.Startup))]
+namespace IssTaskCreator
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
