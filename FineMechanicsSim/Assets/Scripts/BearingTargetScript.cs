045f088e107edb50f3ed9e08a6a27e07ac2b164f
Juraj Doncevic <24619666+JurajDoncevic@users.noreply.github.com>
2017-12-20 14:46:21 +0100

    Update FineMechanicsSim
    Added objects, scripts; Nose behaviour and trigger for bearing;

---
+++ FineMechanicsSim/Assets/Scripts/BearingTargetScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BearingTargetScript : MonoBehaviour {
+
+
+    private Rigidbody rb;
+    private MeshCollider mc;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        mc = GetComponent<MeshCollider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //when the nose fully enters the bearing
+    private void OnTriggerStay(Collider other)
+    {
+        //i don't know how; i don't want to know how; i don't care; it just bloody works!
+        if(mc.bounds.Contains(other.bounds.min) && mc.bounds.Contains(other.bounds.max) && other.gameObject.name == "Nose")
+        {
+            print("point is inside collider");
+        }
+            
+    }
+}
--- FineMechanicsSim/Assets/Scripts/NoseScript.cs
+++ FineMechanicsSim/Assets/Scripts/NoseScript.cs
@@ -1,6 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+
+[Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+
+
+}
 
 public class NoseScript : MonoBehaviour {
 
@@ -12,42 +22,58 @@ public class NoseScript : MonoBehaviour {
     public KeyCode rotLeft = KeyCode.Q;
     public KeyCode rotRight = KeyCode.E;
 
-    public float transForce = 15;
-    public float rotForce = 10;
+    public float transForce = 10;
+    public float rotForce = 5;
 
     private Rigidbody rb;
 
+    public Boundary boundary;
 
     // Use this for initialization
     void Start () {
         rb = this.GetComponent<Rigidbody>();
 
-
+        boundary.xMax = 9.5f;
+        boundary.xMin = -10.0f;
+        boundary.zMax = 10.0f;
+        boundary.zMin = -10.0f;
     }
 	
 	// Update is called once per frame
 	void Update () {
 
-        
+        moveByKeyPress();
 
+        rb.position = new Vector3(
+        Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+        0.0f,
+        Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax));
+
+
+
+    }
+
+
+    private void moveByKeyPress()
+    {
         if (Input.GetKey(transLeft))
-        {
-            rb.velocity = new Vector3(-transForce, 0, 0);
-        }
-
-        if (Input.GetKey(transRight))
-        {
-            rb.velocity = new Vector3(transForce, 0, 0);
-        }
-
-        if (Input.GetKey(transUp))
         {
             rb.velocity = new Vector3(0, 0, transForce);
         }
 
-        if (Input.GetKey(transDown))
+        if (Input.GetKey(transRight))
         {
             rb.velocity = new Vector3(0, 0, -transForce);
+        }
+
+        if (Input.GetKey(transUp))
+        {
+            rb.velocity = new Vector3(transForce, 0, 0);
+        }
+
+        if (Input.GetKey(transDown))
+        {
+            rb.velocity = new Vector3(-transForce, 0, 0);
         }
 
         if (Input.GetKey(rotLeft))
@@ -59,6 +85,5 @@ public class NoseScript : MonoBehaviour {
         {
             rb.angularVelocity = new Vector3(0, rotForce, 0);
         }
-
     }
 }
